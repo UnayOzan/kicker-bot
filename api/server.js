cd8d14a9d70758643e3d265efef300579066df8d
Ozan Unay <unayozan@gmail.com>
2025-07-13 01:01:28 +0300

    main page updated

--- api/server.js
+++ api/server.js
@@ -5,12 +5,20 @@ import cors from "cors";
 const PORT = process.env.PORT || 3000;
 const HOST_URL = process.env.PUBLIC_URL || `http://localhost:${PORT}`;
 
+app.get("/", (req, res) => {
+  res.send("🚀 Kick Bot API Aktif!");
+});
+
 export const startServer = () => {
   const app = express();
   const PORT = 3000;
 
   app.use(cors());
   app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.send("🚀 Kick Bot API Aktif!");
+  });
 
   app.post("/kick-event", (req, res) => {
     const payload = req.body;
