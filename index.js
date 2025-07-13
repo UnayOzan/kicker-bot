56bc3cc7e26329621e9243ce661d607e5bc29dca
unayozan <unayozan@gmail.com>
2025-07-13 18:03:42 +0300

    updated redemptions

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import { startServer } from "./api/server.js";
 import { startListener } from "./api/listener.js";
+import { checkRedemptions } from "./api/checkRedemptions.js";
 
 startServer();
 startListener();
+
+setInterval(checkRedemptions, 10000);
+
 
 console.log("Sistem başlatıldı.");
