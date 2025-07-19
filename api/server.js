e37c35fa05223c65429cd096a373b1c50265b864
unayozan <unayozan@gmail.com>
2025-07-19 19:50:20 +0300

    fixed html issues

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const HOST_URL = process.env.PUBLIC_URL || `http://localhost:${PORT}`;
 const clients = [];
 
 export const startServer = () => {
+  //#region setup
   const app = express();
 
   app.use(cors());
@@ -15,6 +16,31 @@ export const startServer = () => {
 
   app.get("/", (req, res) => {
     res.send("🚀 Kickable Bot API is active!");
+  });
+  //#endregion
+
+  //#region streams
+  app.get("/players-stream", (req, res) => {
+    // Set SSE headers
+    res.set({
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      "Connection": "keep-alive",
+    });
+    res.flushHeaders();
+
+    // Yeni client ekle
+    clients.push(res);
+    console.log(`🟢 Connected observers: ${clients.length}`);
+
+    // İlk data gönder (opsiyonel)
+    res.write(`data: ${JSON.stringify({ gameState })}\n\n`);
+
+    // Client disconnect
+    req.on("close", () => {
+      clients.splice(clients.indexOf(res), 1);
+      console.log(`🔴 Client disconnected. Remaining: ${clients.length}`);
+    });
   });
 
   app.get("/game-stream", (req, res) => {
@@ -32,17 +58,20 @@ export const startServer = () => {
       console.log(`🔴 Client disconnected. Remaining: ${clients.length}`);
     });
   });
+  //#endregion
 
-  app.get("/game-state", (req, res) => {
-    res.json({ gameState });
-  });
-
+  //#region kick
   app.post("/kick-event", (req, res) => {
     const payload = req.body;
     console.log("📩 Incoming Kick event:");
     console.dir(payload, { depth: null });
 
     res.sendStatus(200);
+  });
+  //#endregion
+
+  app.get("/game-state", (req, res) => {
+    res.json({ gameState });
   });
 
   app.listen(PORT, () => {
@@ -51,10 +80,7 @@ export const startServer = () => {
 };
 
 export function broadcast(data) {
-  const json = `Broadcast data:\n${JSON.stringify(data, null, 2)}\n\n`;
-
-  console.log(json);
-
+  const json = `data: ${JSON.stringify(data)}\n\n`;
   for (const client of clients) {
     client.write(json);
   }
