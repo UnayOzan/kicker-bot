fe45c7c46a0f5b3d131b52bfab70a0fe87464516
unayozan <unayozan@gmail.com>
2025-07-23 05:05:37 +0300

    added backend get"/" function

--- src/api/Server.ts
+++ src/api/Server.ts
@@ -16,7 +16,7 @@ export const startServer = (): void => {
 
   app.use(cors());
   app.use(express.json());
-  app.use(express.static(path.join(__dirname, "./public")));
+  app.use(express.static(path.join(__dirname, "../../public")));
 
   app.get("/players-stream", (_req: Request, res: Response) => {
     res.set({
@@ -59,6 +59,10 @@ export const startServer = (): void => {
     console.dir(payload, { depth: null });
 
     res.sendStatus(200);
+  });
+
+  app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
   });
 
   app.get("/game-state", (_req: Request, res: Response) => {
